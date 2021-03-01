03a9cb267d1e5b7f34a5c51c52484b4917393ac6
Mohamed.Elouzati <mohamed.elouzati@student.vives.be>
2021-03-01 17:31:48 +0100

    Connect database

---
+++ ProjectWPL1/TestingPlanner/Models/RqBarcoDivision.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestingPlanner.Models
+{
+    public partial class RqBarcoDivision
+    {
+        public RqBarcoDivision()
+        {
+            RqBarcoDivisionPerson = new HashSet<RqBarcoDivisionPerson>();
+        }
+
+        public string Afkorting { get; set; }
+        public string Alias { get; set; }
+        public bool? Actief { get; set; }
+
+        public virtual ICollection<RqBarcoDivisionPerson> RqBarcoDivisionPerson { get; set; }
+    }
+}
