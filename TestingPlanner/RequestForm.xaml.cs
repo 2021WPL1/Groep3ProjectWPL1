9b84e71ee8c98bc4b5c8ad726c140885fe9ea8dd
Kaalyn <kaatjecactus@gmail.com>
2021-03-01 14:41:42 +0100

    Added screen designs

---
+++ TestingPlanner/RequestForm.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TestingPlanner
+{
+    /// <summary>
+    /// Interaction logic for RequestForm.xaml
+    /// </summary>
+    public partial class RequestForm : Window
+    {
+        public RequestForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TestingPlanner/RequestResources.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TestingPlanner
+{
+    /// <summary>
+    /// Interaction logic for RequestResources.xaml
+    /// </summary>
+    public partial class RequestResources : Window
+    {
+        public RequestResources()
+        {
+            InitializeComponent();
+        }
+    }
+}
