f1a5a94461002a9bcbb19a3b6ac8cb1bd92b8263
Arne <arne.dewintere@student.vives.be>
2021-03-11 09:42:00 +0100

    Jr class made

---
+++ ProjectWPL1/TestingPlanner/Classes/JR.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestingPlanner
+{
+    public class JR
+    {
+        private string jrNumber;
+        private string jrStatus;
+        private string 
+        public JR()
+        {
+
+        }
+    }
+}
