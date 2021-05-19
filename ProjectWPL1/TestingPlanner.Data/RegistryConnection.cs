b72fd264c7a08249a6de0b890fcb70569c9803dc
Kaat <kaatjecactus@gmail.com>
2021-05-19 10:51:21 +0200

    Made RegistryConnection static

--- ProjectWPL1/TestingPlanner.Data/RegistryConnection.cs
+++ ProjectWPL1/TestingPlanner.Data/RegistryConnection.cs
@@ -6,43 +6,27 @@ namespace TestingPlanner.Data
     /// Gets all values from a registry key and stores them in an object
     /// Kaat
     /// </summary>
-    public class RegistryConnection
+    public static class RegistryConnection
     {
-        // String that stores the program subkey with subkeys containing our values
-        private readonly string _mainSubkeyPath;
-
-        /// <summary>
-        /// Constructor for the RegistryConnection class
-        /// </summary>
-        /// <param name="mainSubkeyPath">Relative subkey path within the Current User Key</param>
-        public RegistryConnection(string mainSubkeyPath)
-        {
-            _mainSubkeyPath = mainSubkeyPath;
-        }
-
         /// <summary>
         /// Gets all values from a subkey and stores them in an object
         /// </summary>
-        /// <typeparam name="T">Type that is capable of storing the requested values</typeparam>
-        /// <param name="subkeyName">Name of the subkey storing the requested values</param>
+        /// <typeparam name="T">Type that can store the values in a subkey</typeparam>
+        /// <param name="subkeyPath">Relative path to the subkey, starting at the Current User key</param>
         /// <returns></returns>
-        public T GetValueObject<T>(string subkeyName) where T : new()
+        public static T GetValueObject<T>(string subkeyPath) where T : new()
         {
             var storage = new T();
 
-            using (RegistryKey mainKey = Registry.CurrentUser.OpenSubKey(_mainSubkeyPath))
+            using (RegistryKey valueKey = Registry.CurrentUser.OpenSubKey(subkeyPath))
             {
-                using (RegistryKey valueKey = mainKey.OpenSubKey(subkeyName))
-                {
                     foreach (var property in typeof(T).GetProperties())
                     {
                         property.SetValue(storage, valueKey.GetValue(property.Name));
                     }
-                }
             }
 
             return storage;
         }
-
     }
 }
