8f76132562c0ccf5d4d00a87080b5168e01422be
Kaat Ceusters <kaat.ceusters@student.vives.be>
2021-05-19 11:16:42 +0200

    6 (#18)
    * Loading registry info
    
    * Made RegistryConnection static
    
    * Loading registry data into JR

---
+++ Test/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using TestingPlanner.Data;
+using TestingPlanner.Classes;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var registryCon = new RegistryConnection(@"SOFTWARE\VivesBarco");
+            var user = registryCon.GetValueObject<BarcoUser>("Control");
+
+            Console.WriteLine(user.Name);
+            Console.WriteLine(user.Division);
+            Console.WriteLine(user.Function);
+
+            user = registryCon.GetValueObject<BarcoUser>("Request");
+
+            Console.WriteLine(user.Name);
+            Console.WriteLine(user.Division);
+            Console.WriteLine(user.Function);
+
+            user = registryCon.GetValueObject<BarcoUser>("Test");
+
+            Console.WriteLine(user.Name);
+            Console.WriteLine(user.Division);
+            Console.WriteLine(user.Function);
+        }
+    }
+}
