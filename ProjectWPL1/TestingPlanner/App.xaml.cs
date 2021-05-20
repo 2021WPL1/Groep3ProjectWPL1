e175336bfbcb423760309453cb5235d24418af11
Kaat <kaatjecactus@gmail.com>
2021-05-20 10:58:44 +0200

    Created and redirected to startup windows for different roles

--- ProjectWPL1/TestingPlanner/App.xaml.cs
+++ ProjectWPL1/TestingPlanner/App.xaml.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using TestingPlanner.Data;
+using TestingPlanner.Views;
 
 namespace TestingPlanner
 {
@@ -32,14 +33,17 @@ namespace TestingPlanner
 
             switch (_dao.BarcoUser.Function)
             {
+                case "DEV":
+                    StartWindow = new Temp();
+                    break;
                 case "TEST":
-                    StartWindow = new Temp(); // To do: tester start screen
+                    StartWindow = new StartupTester(); // To do: tester start screen
                     break;
                 case "PLAN":
-                    StartWindow = new Temp(); // To do: Planner start screen
+                    StartWindow = new StartupPlanner(); // To do: Planner start screen
                     break;
                 default:
-                    StartWindow = new Temp(); // To do: general start screen
+                    StartWindow = new StartupRD(); // To do: general start screen
                     break;
             }
 
