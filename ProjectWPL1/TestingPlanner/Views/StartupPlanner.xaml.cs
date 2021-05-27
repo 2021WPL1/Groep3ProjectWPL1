0b573bb4cf90d0290108968a66a3cfa6a6d99de7
DeWintereArne <arne.dewintere@student.vives.be>
2021-05-27 15:42:51 +0200

    setup basic gui + addtolistbox function

--- ProjectWPL1/TestingPlanner/Views/StartupPlanner.xaml.cs
+++ ProjectWPL1/TestingPlanner/Views/StartupPlanner.xaml.cs
@@ -20,6 +20,40 @@ namespace TestingPlanner.Views
         public StartupPlanner()
         {
             InitializeComponent();
+            AddDataToListbox();
+        }
+
+        private void AddDataToListbox()
+        {
+            lstDiv.Items.Add("HC");
+            lstProjectname.Items.Add("MDPC-8127");
+            lstJrnr.Items.Add("000798");
+            lstStatus.Items.Add("Planned");
+            lstForeseendate.Items.Add("29/05/2021");
+
+            lstEnvdiv.Items.Add("HC");
+            lstEnvprojectname.Items.Add("MDPC-7927");
+            lstEnvjrnr.Items.Add("000753");
+            lstEnvstatus.Items.Add("Delayed");
+            lstEnvforeseendate.Items.Add("30/05/2021");
+
+            lstReldiv.Items.Add("HC");
+            lstRelprojectname.Items.Add("MDPC-7428");
+            lstReljrnr.Items.Add("000453");
+            lstRelstatus.Items.Add("Delayed");
+            lstRelforeseendate.Items.Add("02/06/2021");
+
+            lstSafdiv.Items.Add("HC");
+            lstSafprojectname.Items.Add("MDPC-8051");
+            lstSafjrnr.Items.Add("000946");
+            lstSafstatus.Items.Add("Completed");
+            lstSafforeseendate.Items.Add("01/06/2021");
+
+            lstPckdiv.Items.Add("HC");
+            lstPckprojectname.Items.Add("MDPC-7362");
+            lstPckjrnr.Items.Add("000451");
+            lstPckstatus.Items.Add("Planned");
+            lstPckforeseendate.Items.Add("02/06/2021");
         }
     }
 }
