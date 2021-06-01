33796ab513b86d574e98cf80f81e67ccfd9f6124
Kaat <kaatjecactus@gmail.com>
2021-06-01 12:33:41 +0200

    Removed enums again - maybe in following project phase?

--- TestingPlanner.Classes/Enums/StatusJR.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace TestingPlanner.Classes.Enums
-{
-    public enum StatusJR { NotPlanned, Planned, Ongoing, Failed, Passed, DelayedRQ, DelayedPVG, Reported }
-}
