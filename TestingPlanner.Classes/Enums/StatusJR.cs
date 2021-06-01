46aed4222119b0bbfad9a24686cbefbf74010ed1
Kaat <kaatjecactus@gmail.com>
2021-06-01 12:31:40 +0200

    Added enums for JR status

---
+++ TestingPlanner.Classes/Enums/StatusJR.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestingPlanner.Classes.Enums
+{
+    public enum StatusJR { NotPlanned, Planned, Ongoing, Failed, Passed, DelayedRQ, DelayedPVG, Reported }
+}
