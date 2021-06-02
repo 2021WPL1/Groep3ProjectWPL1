3016c3391fe4e22e0208a3890d9c0b066674adcc
DeWintereArne <arne.dewintere@student.vives.be>
2021-06-02 15:32:05 +0200

    add filter function (not working)

--- ProjectWPL1/TestingPlanner/Viewmodels/ViewModelStartupPlanner.cs
+++ ProjectWPL1/TestingPlanner/Viewmodels/ViewModelStartupPlanner.cs
@@ -1,15 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
 using System.Text;
+using System.Windows.Data;
+using TestingPlanner.Domain.Models;
 
 namespace TestingPlanner.Viewmodels
 {
     class ViewModelStartupPlanner: ViewModelCollectionRQ
     {
+        private ICollectionView filterdevisionview;
+        private string filter;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<string> Devisions { get; private set; }
         //Constructor
         public ViewModelStartupPlanner() : base()
         {
             Load();
+            Devisions = new ObservableCollection<string>();
+            filterdevisionview = CollectionViewSource.GetDefaultView(Devisions);
+            filterdevisionview.Filter = s => string.IsNullOrEmpty(filter) ? true : ((string)s).Contains(filter);
         }
 
         // Function used in code behind
@@ -26,6 +39,28 @@ namespace TestingPlanner.Viewmodels
 
             // first JR selected by default --> Selected JR can't be null
             base.SelectedJR = idRequestsOnly[0];
+        }
+        public string FilterDevisions
+        {
+            get
+            {
+                return filter;
+            }
+            set
+            {
+                if(value != filter)
+                {
+                    filter = value;
+                    filterdevisionview.Refresh();
+                    RaisePropertyChanged("filter");
+                }
+            }
+        }
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
