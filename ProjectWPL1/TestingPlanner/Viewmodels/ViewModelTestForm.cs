aa16f9b984f7711a675ed37d0cfe0cf5c5910135
Kaat <kaatjecactus@gmail.com>
2021-06-02 14:59:58 +0200

    UI for planning tests

--- ProjectWPL1/TestingPlanner/Viewmodels/ViewModelTestForm.cs
+++ ProjectWPL1/TestingPlanner/Viewmodels/ViewModelTestForm.cs
@@ -1,10 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using TestingPlanner.Domain.Models;
 
 namespace TestingPlanner.Viewmodels
 {
     class ViewModelTestForm : ViewModelBase
     {
+        // Listbox with equipment
+        public ObservableCollection<string> Resources { get; set; }
+
+        public ObservableCollection<PlPlanning> Tests { get; set; }
+
+        private PlPlanningsKalender selectedTest;
+
+        public ViewModelTestForm()
+        {
+            // Initialize Resources
+            // Initialize Tests
+        }
+
+
+        public PlPlanningsKalender SelectedTest
+        {
+            get => selectedTest;
+            set
+            {
+                selectedTest = value;
+                OnpropertyChanged();
+            }
+        }
+
+
     }
 }
